43fc747c52815c03db74867a94df9473d9e28017
Chris Hodapp <chris.hodapp@urbanalta.com>
2014-11-11 13:26:26 -0500

    Added contact button

--- js/EmployeeView.js
+++ js/EmployeeView.js
@@ -2,7 +2,8 @@ var EmployeeView = function(employee) {
     
     this.initialize = function() {
         this.el = $('<div/>');
-        this.el.on('click', '.add-location-btn', this.addLocation);   
+        this.el.on('click', '.add-location-btn', this.addLocation);
+        this.el.on('click', '.add-contact-btn', this.addToContacts);
     };
     
     this.initialize();
@@ -22,6 +23,26 @@ var EmployeeView = function(employee) {
             function() {
                 alert('Error getting location');
             });
+        return false;
+    };
+    
+    this.addToContacts = function(event) {
+        event.preventDefault();
+        console.log('addToContacts');
+        if (!navigator.contacts) {
+            app.showAlert("Contacts API not supported", "Error");
+            return;
+        }
+        var contact = navigator.contacts.create();
+        contact.name = { givenName: employee.firstName,
+                         familyName: employee.lastName,
+                       };
+        var phoneNumbers = [];
+        phoneNumbers[0] = new ContactField('work', employee.officePhone, false);
+         // preferred number
+        phoneNumbers[1] = new ContactField('mobile', employee.cellPhone, true);
+        contact.phoneNumbers = phoneNumbers;
+        contact.save();
         return false;
     };
 }
